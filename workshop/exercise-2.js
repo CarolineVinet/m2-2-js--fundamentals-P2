9c3c67f5cd8461fdcbe72f2f58af57f20fb38c17
Caroline Vinet <carovinet@gmail.com>
2020-06-16 14:24:56 -0400

    ex 2 reviewed

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -6,11 +6,11 @@
 function max(num1, num2) {
     if (num1 > num2){
         console.log (num1);
-        return num1;
+    
     }
     else if (num2 > num1){
         console.log (num2) ;
-        return num2;
+        
     }
 }
 
